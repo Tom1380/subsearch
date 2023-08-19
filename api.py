4d9a8e34d594ae85a400af92f6a52079bdfb989b
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2023-08-19 23:30:53 +0200

    Added a `search` endpoint to get search results for phrases.

--- api.py
+++ api.py
@@ -3,6 +3,7 @@
 from flask import Flask, Response
 import multiprocessing
 from downloader import downloader_routine
+from search import search_subs
 
 
 def spawn_worker(queue):
@@ -12,16 +13,29 @@ def spawn_worker(queue):
     ).start()
 
 
+def spawn_workers(n, queue):
+    for _ in range(n):
+        spawn_worker(queue)
+
+
 app = Flask(__name__)
 queue = multiprocessing.Queue()
 
-spawn_worker(queue)
+spawn_workers(4, queue)
 
 
 @app.route('/request_download/<id>', methods=['POST'])
 def request_download(id):
     queue.put_nowait(id)
     return Response(status=200)
+
+
+@app.route('/search/<text>', methods=['GET'])
+def search(text):
+    if (search_result := search_subs(text)) is None:
+        return Response(status=404)
+
+    return search_result
 
 
 app.run(debug=True, host='0.0.0.0', port=2000, threaded=True)
--- search.py
+++ search.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, ElasticsearchWarning
+import warnings
+from datetime import datetime
 
-search = input('Your search: ')
+warnings.simplefilter('ignore', ElasticsearchWarning)
 
 es = Elasticsearch(
     hosts="http://localhost:9200",
@@ -10,18 +12,68 @@ es = Elasticsearch(
     verify_certs=False
 )
 
-resp = es.search(
-    index="test-index",
-    query={
-        "query_string": {
-            "query": search
-        }
+
+def search_in_es(text, es):
+    return es.search(
+        index="new2-index",
+        query={
+            "match_phrase": {
+                "subs": {
+                    "query": text,
+                }
+            },
+        },
+        highlight={
+            "fields": {
+                "subs": {
+                    "type": "fvh",
+                }
+            }
+        },
+        size=1,
+    )
+
+
+def find_timestamp(timestamps, index_to_find):
+    for i, (_, current_index) in enumerate(timestamps[1:]):
+        # Because of the slicing
+        i = i + 1
+        prev_timestamp, prev_index = timestamps[i - 1]
+        if prev_index <= index_to_find < current_index:
+            return prev_timestamp
+
+
+def search_subs(text):
+    resp = search_in_es(text, es)
+
+    try:
+        hit = resp['hits']['hits'][0]
+        highlight = hit['highlight']['subs'][0]
+        subs = hit['_source']['subs']
+        timestamps = hit['_source']['timestamps']
+    except:
+        return None
+
+    h = highlight \
+        .replace('<em>', '') \
+        .replace('</em>', '')
+
+    index = subs.find(h)
+    # TODO remove when confident
+    assert h == subs[index:index+len(h)]
+    matching_text = subs[index:index+len(h)]
+    timestamp = find_timestamp(timestamps, index)
+    time = datetime.strptime(timestamp, "%H:%M:%S.%f")
+    # Ignores milliseconds.
+    seconds = time.second + 60 * time.minute + (60**2) * time.hour
+    id = hit['_id']
+    title = hit['_source']['title']
+    link = f'https://youtube.com/watch?v={id}&t={seconds}'
+
+    return {
+        'id': id,
+        'title': title,
+        'time': timestamp,
+        'link': link,
+        'matching_text': matching_text
     }
-)
-
-print(resp)
-for hit in resp['hits']['hits']:
-    print(hit)
-
-
-print("\n\nThese were", len(resp['hits']['hits']), "hits")
