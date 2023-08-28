9e109caa72296ffa7f471d94d95b6f7a80bb35ea
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2023-08-28 21:15:56 +0200

    Cleaned up the code in `find_timestamp`.

--- search.py
+++ search.py
@@ -45,12 +45,15 @@ def search_in_es(text, exact_match, channel_id, es):
 
 
 def find_timestamp(timestamps, index_to_find):
-    for i, (_, current_index) in enumerate(timestamps[1:]):
-        # Because of the slicing
-        i = i + 1
-        prev_timestamp, prev_index = timestamps[i - 1]
-        if prev_index <= index_to_find < current_index:
-            return prev_timestamp
+    previous_timestamp, previous_index = timestamps[0]
+
+    for current_timestamp, current_index in timestamps[1:]:
+        if previous_index <= index_to_find < current_index:
+            return previous_timestamp
+
+        previous_timestamp, previous_index = current_timestamp, current_index
+
+    return previous_timestamp
 
 
 def search_subs(text, exact_match, channel_id):
@@ -95,6 +98,7 @@ def build_match_object(highlight, id, subs, timestamps):
     assert h == subs[index:index+len(h)]
     matching_text = subs[index:index+len(h)]
     timestamp = find_timestamp(timestamps, index)
+    print(timestamp, id, index)
     time = datetime.strptime(timestamp, "%H:%M:%S.%f")
     # Ignores milliseconds.
     seconds = time.second + 60 * time.minute + (60**2) * time.hour
